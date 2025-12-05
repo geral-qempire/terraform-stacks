302834f2fe769b547b65c5970be87f56d4df97b1
qempire <geral.qempire@gmail.com>
2025-12-05 14:54:30 +0000

    Update providers.tf

--- stacks/storage_account_stack/providers.tf
+++ stacks/storage_account_stack/providers.tf
@@ -5,6 +5,14 @@ provider "azurerm" {
   storage_use_azuread = true
 }
 
+provider "azurerm" {
+  alias = "dns"
+
+  features {}
+
+  subscription_id = coalesce(var.dns_subscription_id, var.infra_subscription_id)
+}
+
 provider "azapi" {
   subscription_id = var.infra_subscription_id
 }
