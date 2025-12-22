8b5626cb97e08499e081c85980d1b07aa6364623
qempire <geral.qempire@gmail.com>
2025-12-22 13:09:21 +0000

    delete example stack

--- stacks/example_stack/tests/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-swc-tfstate-nonprod"
-    storage_account_name = "stswcqetfstatenonprod"
-    container_name       = "tfstate"
-    key                  = "stack-staging/example_stack/terraform.tfstate"
-  }
-}
-
